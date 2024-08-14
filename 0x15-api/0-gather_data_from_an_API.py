0629a0992b6fdb3216a861205ab8bd71333ffa4a
Abdu <Abdu>
2024-08-14 04:48:01 -0700

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+or a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    empid = sys.argv[1]
+    baseurl = "https://jsonplaceholder.typicode.com/users"
+    url = baseurl + "/" + empid
+
+    r = requests.get(url)
+    empname = r.json().get('name')
+    todourl = url + "/todos"
+    r = requests.get(todourl)
+    tasks = r.json()
+    done = 0
+    tasks_done = []
+    for task in tasks:
+        if task.get('completed'):
+            tasks_done.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+            .format(empname, done, len(tasks)))
+
+    for task in tasks_done:
+        print("\t {}".format(task.get('title')))
