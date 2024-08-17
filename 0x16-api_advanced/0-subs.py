79adc72ed76bc6adaafc3cd97ca2fad6df0ad0d6
Abdu <Abdu>
2024-08-16 17:17:26 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API
+and returns the number of subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """The function"""
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url)
+    result = r.json()
+
+    try:
+        return result.get('data').get('subcribers')
+    except Exception:
+        return 0
