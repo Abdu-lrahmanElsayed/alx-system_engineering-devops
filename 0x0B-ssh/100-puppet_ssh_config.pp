ebefb92faaa83a3cec1feec08a56ea41de5845f3
Abdu <Abdu>
2024-08-20 07:07:50 -0700

    advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# make changes to our configuration file.
+
+file {'/etc/ssh/ssh_config':
+	ensure => 'present',
+}
+file_line {'Turn off passwd auth':
+	path => '/etc/ssh/ssh_config',
+	line => 'PasswordAuthentication no',
+	match => 'PasswordAuthentication yes',
+	replace => 'true',
+}
+file_line {'Use a Identity file':
+	path => '/etc/ssh/ssh_config',
+        line => 'IdentityFile ~/.ssh/config',
+        match => '^IdentityFile',
+        ensure => 'present',
+}
